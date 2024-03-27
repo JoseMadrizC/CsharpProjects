692391e09e4424f249eebf84be0a64274fe92651
Jose Daniel Madriz Castro <jdmadriz99@gmail.com>
2024-03-27 15:57:30 -0600

    uso de random

---
+++ CsharpProjects/booleanExpressions/Program.cs
@@ -0,0 +1,13 @@
+﻿// Uso de Random
+Console.WriteLine("uso de de random:\n");
+Random coin = new Random();
+int flip = coin.Next(0, 2);
+Console.WriteLine((flip == 0) ? "heads" : "tails");
+
+//desafio 
+string permission = "Admin|Manager";
+int level = 55;
+
+if (permission.Contains("Admin")){
+    Console.WriteLine("a");
+}
